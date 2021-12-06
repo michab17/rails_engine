5db521e3805ed9890c67285ecbffd2e1cf94b376
Micha Bernhard <mib70@pitt.edu>
2021-12-06 16:47:16 -0700

    Test getting one merchant with their id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Requests' do
   before do
-    merchant1 = Merchant.create(name: 'merchant1')
-    merchant2 = Merchant.create(name: 'merchant2')
-    merchant3 = Merchant.create(name: 'merchant3')
+    @merchant1 = Merchant.create(name: 'merchant1')
+    @merchant2 = Merchant.create(name: 'merchant2')
+    @merchant3 = Merchant.create(name: 'merchant3')
   end
   it 'gets a list of all merchants' do
     get '/api/v1/merchants'
 
-    expect(response.status).to eq 200
+    expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
@@ -22,5 +22,19 @@ RSpec.describe 'Merchant Requests' do
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
     end
+  end
+
+  it "can get one merchant by its id" do
+    get "/api/v1/merchants/#{@merchant1.id}"
+  
+    merchant = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(response).to be_successful
+  
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(@merchant1.id)
+  
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
   end
 end
