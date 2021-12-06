eefa9123359d02e38d03c4627b2b6df89dc1a340
Micha Bernhard <mib70@pitt.edu>
2021-12-06 16:57:25 -0700

    Test: ability to get all of a merchants items

---
+++ spec/requests/api/v1/merchants_items_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants Items Requests' do
+  before do
+    @merchant = Merchant.create(name: 'Merchant')
+    @item1 = merchant.items.create(name: 'Item1', description: 'It is an item', unit_price: 5.0)
+    @item2 = merchant.items.create(name: 'Item2', description: 'It is a another item', unit_price: 10.0)
+    @item3 = merchant.items.create(name: 'Item3', description: 'It is yet another item', unit_price: 15.0)
+  end
+  it 'gets all of a merchants items' do
+    get "/api/v1/merchants/#{@merchant.id}/items"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq 3
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(String)
+    end
+  end
+end
