c0cbc14baf844b0844e8ccd568adaa6594723875
Micha Bernhard <mib70@pitt.edu>
2021-12-09 15:10:25 -0700

    Write method to find all merchants by name

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -16,4 +16,14 @@ class Api::V1::MerchantsController < ApplicationController
       end
     end
   end
+
+  def find_all
+    if params[:name]
+      if Merchant.where('name ILIKE ?', "%#{params[:name]}%").first
+        render json: MerchantSerializer.new(Merchant.where('name ILIKE ?', "%#{params[:name]}%"))
+      else
+        render json: {data: []}
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants#find'
+      get '/merchants/find_all', to: 'merchants#find_all'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants_items'
       end
