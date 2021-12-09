156b87db8c64f02d066349a9424692cd41e830d7
Micha Bernhard <mib70@pitt.edu>
2021-12-08 17:59:06 -0700

    Add find method to merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def find
+    if params[:name]
+      if Merchant.where('name ILIKE ?', "%#{params[:name]}%").first
+        render json: MerchantSerializer.new(Merchant.where('name ILIKE ?', "%#{params[:name]}%").first)
+      else
+        render json: {data: {}}
+      end
+    end
+  end
 end
