f645f6b4896381ca7a082b70ecf3829417f53e59
Micha Bernhard <mib70@pitt.edu>
2021-12-08 17:58:45 -0700

    Change names of merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Requests' do
   before do
-    @merchant1 = Merchant.create(name: 'merchant1')
-    @merchant2 = Merchant.create(name: 'merchant2')
-    @merchant3 = Merchant.create(name: 'merchant3')
+    @merchant1 = Merchant.create(name: 'Billy')
+    @merchant2 = Merchant.create(name: 'Kevin')
+    @merchant3 = Merchant.create(name: 'Richie')
   end
   it 'gets a list of all merchants' do
     get '/api/v1/merchants'
@@ -19,7 +19,7 @@ RSpec.describe 'Merchant Requests' do
     end
   end
 
-  it "can get one merchant by its id" do
+  it 'can get one merchant by its id' do
     get "/api/v1/merchants/#{@merchant1.id}"
   
     merchant = JSON.parse(response.body, symbolize_names: true)
@@ -27,5 +27,15 @@ RSpec.describe 'Merchant Requests' do
     expect(response).to be_successful
     expect(merchant[:data][:id]).to eq(@merchant1.id.to_s)
     expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it 'can find one merchant by search' do
+    get "/api/v1/merchants/find?name=Billy"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(merchant[:data][:id]).to eq(@merchant1.id.to_s)
+    expect(merchant[:data][:attributes][:name]).to eq(@merchant1.name)
   end
 end
