336ae5e1e21515754d711c608c2345ef310a2553
Micha Bernhard <mib70@pitt.edu>
2021-12-09 20:06:02 -0700

    Delete comments

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,28 +1,5 @@
 class MerchantSerializer
   include JSONAPI::Serializer
   set_type :merchant
-  attributes :name #, :all_items
-  # has_many :items
-  # def self.format_merchant(merchants)
-  #   { "type": "merchant",
-  #   "properties": {
-  #     "data": { merchants.map do |merchant|
-  #       "type": "merchant",
-  #       "properties": {
-  #         "id": { merchant.id.to_s },
-  #         "type": { "merchant" },
-  #         "attributes": { 
-  #             "properties": {
-  #                 "name": { merchant.name },
-  #             },
-  #             "required": ["name"]
-  #           }
-  #         },
-  #         "required": ["id", "type", "attributes"]
-  #         end
-  #         }
-  #      },
-  #       "required": ['data']
-  #     }
-  # end
+  attributes :name 
 end
