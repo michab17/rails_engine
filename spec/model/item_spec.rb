159524f34e7554449ec67176058c5fca89c32940
Micha Bernhard <mib70@pitt.edu>
2021-12-09 18:06:38 -0700

    Add tests for methods moved from controller

--- spec/model/item_spec.rb
+++ spec/model/item_spec.rb
@@ -26,4 +26,22 @@ RSpec.describe Item, type: :model do
       expect(Item.find_by_min_price("11.0")).to eq(item3)
     end
   end
+
+  describe '::find_all_by_name' do
+    it 'finds all items by their name' do
+      expect(Item.find_all_by_name("P")).to eq([item1, item2])
+    end
+  end
+
+  describe '::find_all_by_max_price' do
+    it 'finds all items by their name' do
+      expect(Item.find_all_by_max_price("14.0")).to eq([item1, item2])
+    end
+  end
+
+  describe '::find_all_by_min_price' do
+    it 'finds all items by their name' do
+      expect(Item.find_all_by_min_price("6.0")).to eq([item2, item3])
+    end
+  end
 end
