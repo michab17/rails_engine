e1545a61bb1148c9a11d1402920657c94713e813
Micha Bernhard <mib70@pitt.edu>
2021-12-14 20:37:09 -0700

    Add routes for new active record methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/merchants/find', to: 'merchants#find'
       get '/merchants/find_all', to: 'merchants#find_all'
+      get '/merchants/most_items', to: 'merchants#most_items'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants_items'
       end
@@ -16,6 +17,14 @@ Rails.application.routes.draw do
       get '/items/find_all', to: 'items#find_all'
       resources :items do
         resources :merchant, only: [:index], controller: 'items_merchants'
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      namespace :revenue do
+        resources :merchants, only: [:index] 
       end
     end
   end
